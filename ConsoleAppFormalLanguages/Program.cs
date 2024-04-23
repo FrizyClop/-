2e3c6f0ce2031c39c4e6655b9d53da6b91a5f6e6
FrizyClop <102551812+FrizyClop@users.noreply.github.com>
2024-04-24 01:58:37 +0300

    Console

---
+++ ConsoleAppFormalLanguages/Program.cs
@@ -0,0 +1,48 @@
+﻿using FormalLanguagesLibrary;
+using System;
+
+namespace ConsoleAppFormalLanguages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            test1();
+            //test2();
+        }
+
+        public static void test1()
+        {
+            Grammar grammar = new Grammar();
+            grammar.AddNonTerminal('A');
+            grammar.AddNonTerminal('B');
+            grammar.AddNonTerminal('C');
+            grammar.AddNonTerminal('F');
+            grammar.AddNonTerminal('L');
+            grammar.AddNonTerminal('4');
+            grammar.AddNonTerminal('Э');
+            grammar.AddNonTerminal('Г');
+            grammar.AddNonTerminal('6');
+            grammar.AddTerminal('1');
+            grammar.AddTerminal('a');
+            grammar.AddTerminal('c');
+            grammar.InstallStartSymbol('A');
+            grammar.AddRule('A', "1B");
+            //grammar.AddRule('B', "cC");
+            grammar.AddRule('C', "a");
+            grammar.AddRule('B', "1A");
+            grammar.AddRule('A', "`");
+            grammar.AddRule('C', "`");
+
+            Console.WriteLine(grammar.ToString());
+
+            StateMachine nda = new StateMachine(grammar);
+            Console.WriteLine(nda.ToString());
+        }
+
+        public static void test2()
+        {
+
+        }
+    }
+}
